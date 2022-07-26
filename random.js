80f59c7176eb6554e73772d274ea3e8a86eb073f
Cheer <84440486+luap580101@users.noreply.github.com>
2022-07-26 23:17:35 +0800

    Create random.js

---
+++ random.js
@@ -0,0 +1,23 @@
+// 每天練習 getTodayPractise(第一題,最後一題,要幾題)
+
+const getTodayPractise = (first, last, num) => {
+  let total = last - first ;
+  const arr = [];
+
+//   console.log(arr.length);
+//   console.log(num);
+
+  while (arr.length !== num) {
+    let newQuestion = Math.round(Math.random() * total +1)
+    console.log(newQuestion);
+    if(arr.includes(newQuestion)){
+        continue;
+    }
+    arr.push(newQuestion);
+    // console.log(arr);
+  }
+  return arr
+};
+// console.log(getTodayPractise(1, 5, 5));
+console.log(getTodayPractise(1, 150, 3));
+
