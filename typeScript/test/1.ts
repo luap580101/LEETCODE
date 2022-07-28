1f5b81f9fbff22d6fc8cc0eb2bdd760a63f81462
Cheer <84440486+luap580101@users.noreply.github.com>
2022-07-28 18:39:05 +0800

    Update 1.ts

--- typeScript/test/1.ts
+++ typeScript/test/1.ts
@@ -130,14 +130,160 @@ const data1: Data = {
   completed: false,
 };
 
-type Beta ={
-    name: string
-}
+type Beta = {
+  name: string;
+};
 
-const beta = data1 as unknown as Beta
+const beta = data1 as unknown as Beta;
 
 // ==========class===========
 
 // private 私有
 // public 公開
-// private 受保護
+// protected 受保護
+
+class Live {
+  roomName: string;
+  private id: string;
+  protected name: string;
+
+  constructor(roomName1: string, id1: string, name1: string) {
+    console.log("建立直播中...");
+
+    this.roomName = roomName1;
+    this.id = id1;
+    this.name = name1;
+  }
+}
+
+class CarLive extends Live {
+  constructor(roomName1: string, id1: string, name1: string) {
+    super(roomName1, id1, name1);
+  }
+
+  start() {
+    // 可以用 name，roomName
+    // id 訪問不到
+  }
+}
+
+const live = new Live("1號", "0001", "Avery");
+const carLive = new CarLive("car room", "0002", "Chen");
+console.log("live", live);
+console.log("carLive", carLive);
+
+class Live2 {
+  // 私有變數
+  #name;
+  constructor(name: string) {
+    this.#name = name;
+  }
+}
+
+const live2 = new Live2("live2");
+// 會找不到name
+// console.log(live2.name);
+
+export interface CarProps {
+  name: string;
+  age: number;
+  start: () => void;
+}
+class Car implements CarProps {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  start() {}
+}
+
+// ===========泛型=========
+
+function print<T>(data: T) {
+  console.log("data", data);
+}
+print<number>(999);
+print<string>("Chen");
+print<boolean>(true);
+
+class Print<T> {
+  data: T;
+
+  constructor(d: T) {
+    this.data = d;
+  }
+}
+const p = new Print<number>(999);
+const p2 = new Print<string>("chen");
+console.log("p", p);
+console.log("p1", p2);
+
+// ============Utility==========
+// Record<Keys, Type>
+interface CatInfo {
+  age: number;
+  breed: string;
+}
+
+type CatName = "miffy" | "boris" | "mordred";
+
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: "Persian" },
+  boris: { age: 5, breed: "Maine Coon" },
+  mordred: { age: 16, breed: "British Shorthair" },
+  // 因為名子沒有定義CHEN 所以會錯
+  // chen: { age: 16, breed: "British Shorthair" },
+};
+
+const obj1: Record<string, boolean> = {
+  name: true,
+};
+
+
+// // Pick<Type, Keys>
+// interface Todo {
+//   title: string;
+//   description: string;
+//   completed: boolean;
+// }
+ 
+// type TodoPreview = Pick<Todo, "title" | "completed">;
+ 
+// const todo: TodoPreview = {
+//   title: "Clean room",
+//   completed: false,
+//   // 沒有引用近來
+//   // description:'123'
+// };
+
+
+// Omit<Type, Keys>
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+  createdAt: number;
+}
+ 
+type TodoPreview = Omit<Todo, "description">;
+ 
+const todo: TodoPreview = {
+  title: "Clean room",
+  completed: false,
+  createdAt: 1615544252770,
+};
+ 
+todo;
+ 
+const todo: TodoPreview
+ 
+type TodoInfo = Omit<Todo, "completed" | "createdAt">;
+ 
+const todoInfo: TodoInfo = {
+  title: "Pick up kids",
+  description: "Kindergarten closes at 5pm",
+};
+ 
